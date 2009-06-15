f5622263c28414ea2f846425cf82b172d4bb343b
Samuel Stauffer <samuel@descolada.com>
2009-06-15 13:20:14 -0700

    Moved munin plugin classes to munin package, and added a memcached_queries plugin

--- munin/postgres.py
+++ munin/postgres.py
@@ -1,50 +1,6 @@
-#!/usr/bin/env python
 
 import os, sys
-
-class MuninPlugin(object):
-    graph_title = ""
-    graph_args = None
-    graph_vlabel = None
-    graph_info = None
-    graph_category = None
-    fields = []
-
-    def __init__(self):
-        super(MuninPlugin, self).__init__()
-
-    def autoconf(self):
-        return False
-
-    def config(self):
-        conf = ["graph_title %s" % self.graph_title]
-        for k in ('graph_category', 'graph_args', 'graph_vlabel', 'graph_info'):
-            v = getattr(self, k)
-            if v:
-                conf.append('%s %s' % (k, v))
-
-        for field_name, field_args in self.fields.iteritems():
-            for arg_name, arg_value in field_args.iteritems():
-                conf.append('%s.%s %s' % (field_name, arg_name, arg_value))
-
-        print "\n".join(conf)
-
-    def suggest(self):
-        sys.exit(1)
-
-    def run(self):
-        cmd = (sys.argv[1] if len(sys.argv) > 1 else None) or "execute"
-        if cmd == "execute":
-            self.execute()
-        elif cmd == "autoconf":
-            if not self.autoconf():
-                print "no"
-            print "yes"
-        elif cmd == "config":
-            self.config()
-        elif cmd == "suggest":
-            self.suggest()
-        sys.exit(0)
+from munin import MuninPlugin
 
 class MuninPostgresPlugin(MuninPlugin):
     dbname_in_args = False
