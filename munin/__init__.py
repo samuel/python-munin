389da2b5c14d682697a96a5bd4fdf575099e5521
Samuel Stauffer <samuel@descolada.com>
2009-08-27 01:50:51 -0500

    Added MuninClient class.. though right now it's a bit of a hack

--- munin/__init__.py
+++ munin/__init__.py
@@ -3,6 +3,7 @@ __version__ = "1.0.1"
 
 import os
 import sys
+import socket
 
 class MuninPlugin(object):
     title = ""
@@ -54,3 +55,30 @@ class MuninPlugin(object):
         elif cmd == "suggest":
             self.suggest()
         sys.exit(0)
+
+class MuninClient(object):
+    def __init__(self, host, port=4949):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+        self.sock.recv(4096) # welcome, TODO: receive all
+
+    def _command(self, cmd, term):
+        self.sock.send("%s\n" % cmd)
+        buf = ""
+        while term not in buf:
+            buf += self.sock.recv(4096)
+        return buf.split(term)[0]
+
+    def list(self):
+        return self._command('list', '\n').split(' ')
+
+    def fetch(self, service):
+        data = self._command("fetch %s" % service, ".\n")
+        if data.startswith('#'):
+            raise Exception(data[2:])
+        values = {}
+        for line in data.split('\n'):
+            if line:
+                k, v = line.split(' ', 1)
+                values[k.split('.')[0]] = Decimal(v)
+        return values
