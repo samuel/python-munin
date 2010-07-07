aedc7be6e96f099025a82a9f1df1261c858134be
Samuel Stauffer <samuel@descolada.com>
2010-07-06 18:49:22 -0700

    Added a cassandra load plugin

--- munin/cassandra.py
+++ munin/cassandra.py
@@ -46,3 +46,25 @@ class MuninCassandraPlugin(MuninPlugin):
 
     def cfstats(self):
         return self.parse_cfstats(self.execute_nodetool("cfstats"))
+
+    def cinfo(self):
+        text = self.execute_nodetool("info")
+        lines = text.strip().split('\n')
+        token = lines[0]
+        info = {}
+        for l in lines[1:]:
+            name, value = l.split(':')
+            info[name.strip()] = value.strip()
+        l_num, l_units = info['Load'].split(' ', 1)
+        l_num = float(l_num)
+        if l_units == "KB":
+            scale = 1024
+        elif l_units == "MB":
+            scale = 1024*1024
+        elif l_units == "GB":
+            scale = 1024*1024*1024
+        elif l_units == "TB":
+            scale = 1024*1024*1024*1024
+        info['Load'] = int(l_num * scale)
+        info['token'] = token
+        return info
