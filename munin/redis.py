be58e7e2cd07eec65579d85f61135843f6ba22a7
Samuel Stauffer <samuel@descolada.com>
2010-10-17 10:05:53 -0700

    Added a redis active connections plugin

---
+++ munin/redis.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import socket
+from munin import MuninPlugin
+
+class MuninRedisPlugin(MuninPlugin):
+    category = "Redis"
+
+    def autoconf(self):
+        try:
+            self.get_info()
+        except socket.error:
+            return False
+        return True
+
+    def get_info(self):
+        host = os.environ.get('REDIS_HOST') or '127.0.0.1'
+        port = int(os.environ.get('REDIS_PORT') or '6379')
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send("*1\r\n$4\r\ninfo\r\n")
+        buf = ""
+        while '\r\n\r\n' not in buf:
+            buf += s.recv(1024)
+        s.close()
+        return dict(x.split(':', 1) for x in buf.split('\r\n')[1:-2])
+
+    def execute(self):
+        stats = self.get_info()
+        for k, v in self.fields:
+            try:
+                value = stats[k]
+            except KeyError:
+                value = "U"
+            print "%s.value %s" % (k, value)
